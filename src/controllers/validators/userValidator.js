cf46f49e4af206f147eed7008f104dfe4031627b
Avinash <avinashb97@gmail.com>
2019-01-20 21:28:39 +0530

    Setup User Validator

---
+++ src/controllers/validators/userValidator.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+const User = require('../../models/user');
+
+const userWithIdExists = async (userId) => {
+  let user;
+  try {
+    user = await User.find({ userId });
+  } catch (error) {
+    throw error;
+  }
+  if (user.length > 0) {
+    return true;
+  }
+  return false;
+};
+
+const ValidateCreate = Joi.object().keys({
+  userId: Joi.string().min(3).max(30).required()
+});
+
+const create = async (req, res, next) => {
+  const { error, value } = ValidateCreate.validate(req.body);
+  if (error) {
+    console.log(error);
+    res.status(400).json({
+      message: 'Invalid User Id'
+    });
+    return;
+  }
+
+  if (await userWithIdExists(value.userId)) {
+    res.status(400).json({
+      message: 'This user id is already registered with another user'
+    });
+    return;
+  }
+
+  req.parsed = value;
+  next();
+};
+
+module.exports = { create };
